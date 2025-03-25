1a0c11ed44d13c4c30f7d239b0686127b6dc6c22
Christian DerManuelian <christian.dermanuelian@gmail.com>
2025-03-24 19:48:01 -0700

    Nav Bar and static banner image

---
+++ app/Navigation/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <nav className="h-[100px] flex items-center justify-center">
+            Institute of Risk and Safety Analysis
+        </nav>
+    );
+};
+
+export default NavBar;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div>
+        <div className="w-full flex flex-col items-center">
+            <div className="w-full h-[700px] overflow-hidden relative">
+                <Image
+                    src="/image/home/edr.jpg"
+                    alt={"home image"}
+                    fill
+                    className="object-cover"
+                />
+            </div>
+
             <h1>Help me</h1>
         </div>
     );
