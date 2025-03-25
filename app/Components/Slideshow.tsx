1a732f11c0c98c0b996a4052bb1db4b27fb048bd
Christian DerManuelian <christian.dermanuelian@gmail.com>
2025-03-25 15:06:29 -0700

    Crossfade Slideshow

--- app/Components/Slideshow.tsx
+++ app/Components/Slideshow.tsx
@@ -9,10 +9,15 @@ interface Params {
 
 const Slideshow = ({ images, intervalDelay = 5000 }: Params) => {
     const [index, setIndex] = useState(0);
+    const [isFading, setFading] = useState(false);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setIndex((prev) => (prev + 1) % images.length);
+            setFading(true);
+            setTimeout(() => {
+                setIndex((prev) => (prev + 1) % images.length);
+                setFading(false);
+            }, 2000);
         }, intervalDelay);
 
         return () => clearInterval(interval);
@@ -21,20 +26,26 @@ const Slideshow = ({ images, intervalDelay = 5000 }: Params) => {
     return (
         <div className="w-full h-full relative">
             <div className="flex w-full h-full absolute">
-                <Image
-                    src={images[index].path}
-                    alt={images[index].desc}
-                    height={200}
-                    width={800}
-                    className="object-cover"
-                />
-                <Image
-                    src={images[(index + 1) % images.length].path}
-                    alt={images[(index + 1) % images.length].desc}
-                    height={200}
-                    width={800}
-                    className="object-cover"
-                />
+                {images.map((image, i) => (
+                    <Image
+                        src={image.path}
+                        alt={image.desc}
+                        fill
+                        className="object-cover slideshowImage"
+                        style={{
+                            opacity:
+                                i === index
+                                    ? isFading
+                                        ? 0
+                                        : 1
+                                    : i === (index + 1) % images.length &&
+                                      isFading
+                                    ? 1
+                                    : 0,
+                        }}
+                        key={"home_image_" + i}
+                    />
+                ))}
             </div>
 
             <div className="tint w-full h-full absolute" />
